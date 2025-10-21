eb4f17e64f71e6470d1b65e2f7791775e3ca4049
MiguelAHernandez <miguelangelHDV@gmail.com>
2025-10-21 18:49:03 -0500

    TiendaSteam

--- 13.tiendaSteam/script.js
+++ 13.tiendaSteam/script.js
@@ -1,7 +1,8 @@
 agregarP=document.getElementById("agregarP")
 agregarE=document.getElementById("agregarE")
 agregarC=document.getElementById("agregarC")
-canasta=document.getElementById("canasta")
+canasta=document.getElementById("list")
+tot=document.getElementById("res")
 carrito=[
     {name:"Pizza", price:2000, count:0},
     {name:"Empanada", price:4000, count:0},
@@ -10,19 +11,19 @@ carrito=[
 
 function actualizar() {
     total=0
-    html="<h2>Carrito</h2>"
+    html=""
     carrito.forEach((objeto,indice) => {
         if (objeto.count>0){
             total=total+(objeto.price*objeto.count)
-            html+=`<div class="estudiante">
+            html+=`<div class="box">
             <p>${objeto.count} ${objeto.name}</p>
             <p>${objeto.price}</p>
             <button class="borrar" onClick="subtract(${indice})">
-            Eliminar</button>
+            <img src="imagenes/trash.png"></button>
             </div>`
         }else if(objeto.count<0){objeto.count=0}
     })
-    html+=`<p id="res">Total: ${total}</p>`
+    tot.innerHTML=`Total: ${total}`
     canasta.innerHTML=html
 }
 function add(indice){
